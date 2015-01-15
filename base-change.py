5198c127fe294bd8b7b8b2e528320300d526a6e2
abhiShandy <abhishek.sandy@gmail.com>
2015-01-15 12:18:58 +0530

    basic input/output

---
+++ base-change.py
@@ -0,0 +1,6 @@
+inputNum = raw_input('Enter the number to be converted: ')
+inputBase = int(raw_input('Enter the base of the input: '))
+outputBase = int(raw_input('Enter the desired base of the output: '))
+
+print 'You entered: ', inputNum, ' of base ', inputBase, ' which should be converted to ', outputBase
+
