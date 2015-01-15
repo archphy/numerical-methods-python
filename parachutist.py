261558ee607add3e7194984632c33b1caa984f6d
abhiShandy <abhishek.sandy@gmail.com>
2015-01-15 12:10:08 +0530

    initial commit

---
+++ parachutist.py
@@ -0,0 +1,9 @@
+v_0 = 0
+g = 9.8
+c = 12.5
+m = 68.1
+ti = float(raw_input("Enter Time Interval in seconds:"))
+print 'Time Interval entered:', ti, "seconds"
+tf = float(raw_input("Enter Finishing Time in seconds:"))
+print 'Finish Time entered:', tf, "seconds"
+for i in ran
