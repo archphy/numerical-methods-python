69a641e23982055b0a96efcad7456382f72fe262
abhiShandy <abhishek.sandy@gmail.com>
2015-02-04 18:30:35 +0530

    formatting numerals[]

--- base-change.py
+++ base-change.py
@@ -1,4 +1,8 @@
-numerals = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y' ,'Z']
+numerals = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, \
+			'A', 'B', 'C', 'D', 'E', 'F', 'G',\
+			'H', 'I', 'J', 'K', 'L', 'M', 'N',\
+			'O', 'P', 'Q', 'R', 'S', 'T', 'U',\
+			'V', 'W', 'X', 'Y' ,'Z']
 def map_digits(dig):
 	return numerals[dig]
 
@@ -62,11 +66,13 @@ def test_all():
 	print '7B from base-16 to base-10:', base_change('7B', 16, 10)
 	print '=========='
 	
-test_all()
+# test_all()
 
 inputNum = raw_input('Enter the number to be converted: ')
 inputBase = int(raw_input('Enter the base of the input: '))
 outputBase = int(raw_input('Enter the desired base of the output: '))
 
+print '----------'
 print 'You wanted', inputNum, 'of base:', inputBase, 'to be converted to base:', outputBase
 print 'Answer: ', base_change(inputNum, inputBase, outputBase)
+print '----------'
