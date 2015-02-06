99782232ddcf7c368346fe0a11cc4a73f649a2b9
abhiShandy <abhishek.sandy@gmail.com>
2015-02-07 01:10:42 +0530

    gauss jordan elimination

--- gauss_elimination.py
+++ gauss_elimination.py
@@ -4,13 +4,15 @@ from numpy import array
 from numpy import zeros
 import matrix_operations as mo
 # row scaling of the matrix
-def row_scaling(a):
+def row_scaling(a,take_abs=True):
 	n = a.shape[0]
 	max_row = 0
 	for i in xrange(0,a.shape[0]):
 		for j in xrange(0,n):
-			if max_row < abs(a[i][j]):
+			if take_abs and max_row < abs(a[i][j]):
 				max_row = abs(a[i][j])
+			elif not take_abs and abs(max_row) < abs(a[i][j]):
+				max_row = a[i][j]
 		if max_row != 0:
 			for j in xrange(0,a.shape[1]):
 				a[i][j] /= max_row
@@ -25,10 +27,8 @@ def partial_pivoting(a):
 def transform_to_row_echleon(a):
 	n = a.shape[0]
 	for i in xrange(0,n-1):
-		# print i, ':'
 		for j in xrange(i+1,n):
 			factor = a[j][i]/a[i][i]
-			# print factor
 			for k in xrange(0,n+1):
 				a[j][k] -= factor*a[i][k]
 	return a
@@ -75,7 +75,6 @@ def solve(a,b):
 # test
 # A = array([[2,3,-1],[4,4,-3],[-2,1,-1]])
 # B = mo.transpose(array([5,3,-3]))
-A = array([[4,1],[1,3]])
-B = mo.transpose(array([1,2]))
-solve(A,B)
-# print B
+# A = array([[4,1],[1,3]])
+# B = mo.transpose(array([1,2]))
+# solve(A,B)
---
+++ gauss_jordan_elimination.py
@@ -0,0 +1,49 @@
+import gauss_elimination as ge
+import matrix_operations as mo
+from numpy import array
+from numpy import zeros
+
+# transform to identity matrix
+def transform_to_identity(a):
+	a = ge.transform_to_row_echleon(a)
+	n = a.shape[0]
+	for i in xrange(1,n):
+		print i
+		for j in xrange(0,i):
+			factor = a[j][i]/a[i][i]
+			for k in xrange(0,n+1):
+				a[j][k] -= factor*a[i][k]
+	a = ge.row_scaling(a, False)
+	return a
+
+# abstracting solution
+def abstract_solution(a):
+	n = a.shape[0]
+	p = zeros(n)
+	for i in xrange(0,n):
+		p[i] = a[i,-1]
+	return p
+
+# solve a set of equation
+def solve(a,b):
+	print "Coefficients:\n", a
+	print "Values:\n", b
+	p = mo.augmented_matrix(a,b)
+	print 'Augmented:\n', p
+	p = ge.row_scaling(p)
+	print 'Row scaled:\n', p
+	# p = partial_pivoting(p)
+	# print "Partially pivoted:\n", p
+	p = transform_to_identity(p)
+	print 'Transformed to identity form:\n', p
+	# q = backward_substitution(p)
+	# print "On backward substitution:\n", q
+	q = abstract_solution(p)
+	print "On abstracting solution:\n", q
+
+# test
+A = array([[2,3,-1],[4,4,-3],[-2,1,-1]])
+B = mo.transpose(array([5,3,-3]))
+# A = array([[4,1],[1,3]])
+# B = mo.transpose(array([1,2]))
+solve(A,B)
