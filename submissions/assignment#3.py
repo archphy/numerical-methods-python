7acd7a56e5862b5844cf6554807f1896a9f67c70
abhiShandy <abhishek.sandy@gmail.com>
2015-02-19 21:40:26 +0530

    submitting assignment#3

--- submissions/assignment#3.py
+++ submissions/assignment#3.py
@@ -1,26 +1,8 @@
-# algorithm#3.3
-# Assignment #3
-# tasks left:
-# 1. check for symmetry 				done
-# 2. check for NxN						done
-# 3. minimize storage					done
-# 4. bring all functions in one program	done
-# 5. check for positive definite 		done
-# 6. solve a test case 					done
-# 7. document and submit
-# 8. input array from terminal			done
-# 9. minimize the number of inputs 		done
-# 10. validation for gibberish input
-
-# import time
-from numpy import array
-from numpy import zeros
-# from numpy import identity
-from numpy import arange
-from numpy import dot
-from math import sqrt
-
-# start_time = time.time()
+from numpy import array # array objects
+from numpy import zeros # null arrays
+from numpy import arange # arrays with serialized elements
+from numpy import dot # dot product of arrays
+from math import sqrt # square root function
 
 def transpose(a):
 	if not check_1D(a):
@@ -34,12 +16,12 @@ def transpose(a):
 
 def check_1D(a):
 	try:
-		a.shape[1]
+		a.shape[1] # throws IndexError if the array is 1D
 		return False
 	except IndexError:
 		return True
 
-def matrix_equality(a,b):
+def matrix_equality(a,b): # compares to arrays element-wise
 	c = (a==b)
 	for k in c:
 		for j in k:
@@ -49,7 +31,7 @@ def matrix_equality(a,b):
 
 def check_positive_definite(a):
 	global n
-	X = arange(n)
+	X = arange(n) # 1D array with elements as 0,1,2....(n-1)
 	Y = dot(dot(X,a),transpose(X)) > 0
 	return Y[0]
 
@@ -66,22 +48,16 @@ def forward_substitution(a,b):
 	return x
 
 def backward_substitution(a,b):
-	# currently supports only (n x n+1) matrix
 	global n
 	p = zeros(n)
-	sum_1 = 0
-
-	# last variable
+	sum_of_others = 0
 	p[-1] = b[-1][-1] / a[-1][-1];
-	
-	# other variables
 	for i in xrange(n-2,-1,-1):
 		for j in xrange(n-1,i,-1):
-			sum_1 += a[i][j]*p[j]
+			sum_of_others += a[i][j]*p[j]
 		
-		p[i] = (b[i][0] - sum_1) / a[i][i]
-		sum_1 = 0
-	
+		p[i] = (b[i][0] - sum_of_others) / a[i][i]
+		sum_of_others = 0	
 	return transpose(p)
 
 def choleski_decomposition():
@@ -105,17 +81,12 @@ def solve():
 	global A,B
 	if not check_positive_definite(A):
 		print 'A matrix must be positive definite'
+		accept_inputs()
 	else:
 		choleski_decomposition()
 		y = forward_substitution(A,B)
 		s = backward_substitution(A,y)
 		print 'Solution matrix:\n', s
-		# print 'Modified A matrix:\n', A
-		# print 'Lower triangular matrix:\n', l
-		# print 'Upper triangular matrix:\n', u
-		# print 'On forward substitution:\n', y
-		# print u.shape, y.shape, b.shape
-		# print 'On backward substitution:\n', s
 
 def accept_coefficients():
 	global A
@@ -123,6 +94,7 @@ def accept_coefficients():
 	n = input('Enter the number of variables(n): ')
 	A = zeros((n,n))
 	print 'Enter the value for:'
+	# ensures that A matrix is symmetric and reduces number of inputs required
 	for x in xrange(n):
 		for y in xrange(x,n):
 			A[x][y] = input('A[%d][%d]: ' % (x+1,y+1))
@@ -137,26 +109,12 @@ def accept_values():
 	for x in xrange(n):
 		B[x][0] = input('B[%d][%d]: ' % (x+1,1))
 
-
 def accept_inputs():
-	# accept_coefficients()
-	# accept_values()
-	# print 'Number of variables:', n
-	# print 'A matrix\n', A
-	# print 'B matrix\n', B
-	global A,B,n
-	A = array([[1.44,-0.36,5.52,0],[-0.36,10.33,-7.78,0],[5.52,-7.78,28.4,9],[0,0,9,61]], dtype="float")
-	B = transpose(array([0.04,-2.15,0,0.88]))
-	n = 4
-	# print check_positive_definite(A)
+	accept_coefficients()
+	accept_values()
+	print 'Number of variables:', n
+	print 'A matrix\n', A
+	print 'B matrix\n', B
+	solve()
 
 accept_inputs()
-solve()
-
-# test
-# solution: [3.0921,-0.7387,-0.8476,0.1395]
-# solve(A,B)
-# print 'Time taken: %.4f seconds' % (time.time() - start_time)
-# from guppy import hpy
-# h = hpy()
-# print h.heap()
